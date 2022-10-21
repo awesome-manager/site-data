6517e299726ef5fab48b654addcd95051cb6b8b0
Lebedev Vadim <v.lebedev@qsoft.ru>
2022-10-21 20:32:16 +0800

    Refactor project controllers

--- app/Http/Controllers/Pages/Projects/ProjectController.php
+++ app/Http/Controllers/Pages/Projects/ProjectController.php
@@ -2,24 +2,50 @@
 
 namespace App\Http\Controllers\Pages\Projects;
 
+use App\Http\Controllers\Controller;
+use Awesome\Foundation\Traits\Arrays\Arrayable;
+use Awesome\Foundation\Traits\Requests\Decoding;
 use App\Http\Resources\Pages\Projects\ProjectsResource;
+use AwesomeManager\ProjectService\Client\Facades\ProjectClient;
 
-class ProjectController extends AbstractProjectController
+class ProjectController extends Controller
 {
+    use Arrayable, Decoding;
+
     public function data()
     {
         $projects = $this->findProjects();
 
         $this->abortIf(empty($projects));
 
-        $statuses = $this->findStatuses(array_unique(array_column($projects, 'status_id')));
+        $statuses = $this->findStatuses($this->pluckUniqueColumn($projects, 'status_id'));
 
-        $groups = $this->findGroups(array_unique(array_column($projects, 'group_id')));
+        $groups = $this->findGroups($this->pluckUniqueColumn($projects, 'group_id'));
 
-        $customers = $this->findCustomers(array_unique(array_column($projects, 'customer_id')));
+        $customers = $this->findCustomers($this->pluckUniqueColumn($projects, 'customer_id'));
 
         return response()->jsonResponse(new ProjectsResource(
             collect(compact('projects', 'statuses', 'groups', 'customers'))
         ));
+    }
+
+    private function findProjects()
+    {
+        return $this->decode(ProjectClient::projects()->send(), 'projects', []);
+    }
+
+    private function findStatuses(array $ids = [])
+    {
+        return $this->decode(ProjectClient::statuses(['ids' => $ids])->send(), 'statuses', []);
+    }
+
+    private function findGroups(array $ids = [])
+    {
+        return $this->decode(ProjectClient::groups(['ids' => $ids])->send(), 'groups', []);
+    }
+
+    private function findCustomers(array $ids = [])
+    {
+        return $this->decode(ProjectClient::customers(['ids' => $ids])->send(), 'customers', []);
     }
 }
